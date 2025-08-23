faf9bc356473c0c6c881836662e78972a7c73f3a
martingutoli <martingutoli@gmail.com>
2025-08-23 20:14:22 -0300

    Subo práctica unidad 1 y readme actualizado

---
+++ Unidad 1/Practico 1 Estructuras secuenciales.py
@@ -0,0 +1,96 @@
+#1) 
+print("Hola Mundo!")
+
+# # 2)
+# nombre = input("ingrese su nombre: ")
+# print(f"hola {nombre}")
+
+# 3) nombre = input("ingrese su nombre:")
+# apellido = input("ingrese su apellido")
+# edad = int(input("ingrese su edad: "))
+# lugarResidencia = input("ingrese el lugar donde vive: ")
+# print(f"soy {nombre} {apellido}, tengo {edad} años y vivo en {lugarResidencia}")
+
+# 4)radio = int(input("ingrese el radio: "))
+# areaCirc = 3.14*(radio*radio)
+# perimetroCirc = 2*3.14*radio
+# print(f"el area del circulo es: {areaCirc}, y el perimetro es: {perimetroCirc}")
+
+# 5)segundos = int(input("ingrese la cantidad de segundos que desee calcular: "))
+# horas = segundos/3600
+# print(f"los segundos ingresados son: {segundos} y el equivalente en horas es: {horas}")
+
+#  6)
+#  num = int(input("ingrese un numero: "))
+#  resultadox0 = num*0 
+#  resultadox1 = num*1
+#  resultadox2 = num*2
+#  resultadox3 = num*3
+#  resultadox4 = num*4
+#  resultadox5 = num*5
+#  resultadox6 = num*6
+#  resultadox7 = num*7
+#  resultadox8 = num*8
+#  resultadox9 = num*9
+#  resultadox10 = num*10
+
+#  print(f"""
+#        0*{num}={resultadox0}
+#        1*{num}={resultadox1}
+#        2*{num}={resultadox2}
+#        3*{num}={resultadox3}
+#        4*{num}={resultadox4}
+#        5*{num}={resultadox5}
+#        6*{num}={resultadox6}
+#        7*{num}={resultadox7}
+#        8*{num}={resultadox8}
+#        9*{num}={resultadox9}
+#        10*{num}={resultadox10}
+#  """)
+
+# 7)
+#  num1 = int(input("ingrese el primer numero distinto de 0: "))
+#  num2 = int(input("ingrese el segundo numero distinto de 0: "))
+#  suma = num1+num2
+#  resta = num1-num2
+#  multi = num1*num2
+#  division = num1/num2
+#  print(f"""
+#        {num1} + {num2} = {suma}
+#        {num1} - {num2} = {resta}
+#        {num1} * {num2} = {multi}
+#        {num1} / {num2} = {division}
+#        """)
+
+# 8)
+
+#  altura = float(input("ingrese su altura en mts: "))
+#  peso = int(input("ingrese su peso en kg: "))
+#  imc = int(peso/(altura*altura))
+#  print(f"su imc es: {imc}")
+
+# 9)
+#  tempCelsius = int(input("ingrese la temperatura en grados celsius: "))
+#  tempFaren = 1.8*tempCelsius+32
+#  print(f"el equivalante de celsius a farenheit es: {tempFaren}")
+
+# 10)
+
+#  num1 = int(input("ingrese el primera numero: "))
+#  num2 = int(input("ingrese el segundo numero: "))
+#  num3 = int(input("ingrese el tercer numero: "))
+#  promedio = (num1+num2+num3)/3
+#  print(f"el promedio es: {promedio}")
+
+# b= 5
+# c = b+3
+# a= b*c
+# print(f"{a}, {b}, {c}")
+
+# num1=10
+# rango_minimo=20
+# numero_usuario=int(input("ingrese un numero: "))
+# dentro_rango=(rango_minimo<= num1 and numero_usuario<=rango_minimo)
+# print(dentro_rango)
+
+
